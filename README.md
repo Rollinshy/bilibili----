3199aa2d494dc0431079855917dd89ec817f888d
shy <2759643667@qq.com>
2022-05-05 15:50:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# webProjectDesign
+web前端开发课程设计  
+主页的文件名 index.html  
+注册界面的文件名 register.html 
+登录界面的文件名 login.html  
+番剧的文件名 fanju.html
+游戏中心的文件名 game.html
+直播的文件名 live.html
+会员购的文件名 shopping.html
+漫画的文件名 comic.html  
+搜索界面的文件名 search.html  
+文章列表页(list.html)
+文章内容页(content.html)
+个人信息设置页(setting.html)
+个人简历介绍页(resume.html)
+内容展示列表页(content_list.html)
+表格展示页(table.html)
+个人常用链接展示页(links.html)。
---
+++ js/video/comment.js
@@ -0,0 +1,32 @@
+var button = document.getElementById("right");
+var commentList = document.getElementById("comment-list");
+var comment = document.getElementById("comment-content");
+button.onclick = function() {
+	if (comment.value.trim() != "") {
+		var li = document.createElement("li");
+		var img = document.createElement("img");
+		var userComment = document.createElement("div");
+		var div1 = document.createElement("div");
+		var div2 = document.createElement("div");
+		var div3 = document.createElement("div");
+		var span = document.createElement("span");
+		var lv = document.createElement("img");
+		img.src = "images/video/头像.jpg";
+		img.className = "user-image";
+		userComment.className = "user-comment";
+		span.innerHTML = "匿名用户";
+		lv.src = "images/video/lv5.png";
+		div1.appendChild(span);
+		div1.appendChild(lv);
+		div2.innerHTML = comment.value.trim();
+		div3.innerHTML = "来自网页端&nbsp;&nbsp;&nbsp;" + timeNow;
+		userComment.appendChild(div1);
+		userComment.appendChild(div2);
+		userComment.appendChild(div3);
+		li.appendChild(img);
+		li.appendChild(userComment);
+		commentList.appendChild(li);
+		comment.value = "";
+	}
+}
+// }
